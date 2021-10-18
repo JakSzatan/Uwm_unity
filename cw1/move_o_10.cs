e7e8276a47b9213b29849724c6661de49bad4ae9
JakSzatan <47736299+JakSzatan@users.noreply.github.com>
2021-10-18 15:22:49 +0200

    Update move_o_10.cs

--- cw1/move_o_10.cs
+++ cw1/move_o_10.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class move_o_10 : MonoBehaviour
 {
     public float speed;
+    private float startX;
+    
+    void Start(){
+        startX = transform.position.x;
+    
+    }
     // Update is called once per frame
-    void Update()
-    {
-        if (transform.position.x > 5.0f || transform.position.x < -5.0f)
+    void Update(){
+        if (speed>0&&startX+ 10 < transform.position.x)
         {
-            speed *=-1;
+            speed *= -1;
+        }
+        if (speed < 0 && startX > transform.position.x)
+        {
+            speed *= -1;
         }
         transform.Translate(speed, 0, 0);
-        
     }
 }
