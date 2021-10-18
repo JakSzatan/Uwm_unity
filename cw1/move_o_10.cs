bc81c9797e737f5fd62504d821438f953c147b62
JakSzatan <47736299+JakSzatan@users.noreply.github.com>
2021-10-18 14:57:25 +0200

    Add files via upload

---
+++ cw1/move_o_10.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_o_10 : MonoBehaviour
+{
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 5.0f || transform.position.x < -5.0f)
+        {
+            speed *=-1;
+        }
+        transform.Translate(speed, 0, 0);
+        
+    }
+}
