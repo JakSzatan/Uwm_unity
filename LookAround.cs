9cfe61e6478576db874524bbcfad13cfe572c26a
JakSzatan <47736299+JakSzatan@users.noreply.github.com>
2021-10-27 15:47:53 +0200

    Add files via upload

---
+++ LookAround.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    // ruch wokół osi Y będzie wykonywany na obiekcie gracza, więc
+    // potrzebna nam referencja do niego (konkretnie jego komponentu Transform)
+    public Transform player;
+
+    public float sensitivity = 200f;
+    private float mouseYMove = 0;
+    void Start()
+    {
+        // zablokowanie kursora na środku ekranu, oraz ukrycie kursora
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // pobieramy wartości dla obu osi ruchu myszy
+        float mouseXMove = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseYMove += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        player.Rotate(Vector3.up * mouseXMove);
+
+        // a dla osi X obracamy kamerę dla lokalnych koordynatów
+        // -mouseYMove aby uniknąć ofektu mouse inverse
+
+        mouseYMove = Mathf.Clamp(mouseYMove, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(-mouseYMove, 0f, 0f);
+
+    }
+}
